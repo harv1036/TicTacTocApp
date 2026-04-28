d435a00c2893112c1142833d917b37a168653da5
Harsh Jain <harv1032006@gmail.com>
2026-04-29 00:10:42 +0530

    UC5: Validate User Move

---
+++ UC5.java
@@ -0,0 +1,19 @@
+public class UC5 {
+    static char[][] board={
+        {'-','-','-'},
+        {'-','-','-'},
+        {'-','-','-'}
+    };
+    public static void main(String[] args){
+        System.out.println(isValidMove(1,1));
+    }
+    public static boolean isValidMove(int row, int col){
+        if(row<0 || row>2 || col<0 || col>2){
+            return false;
+        }
+        if(board[row][col]!='-'){
+            return false;
+        }
+        return true;
+    }
+}
